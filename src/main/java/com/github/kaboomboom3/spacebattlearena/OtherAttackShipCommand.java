2ab0e6b821c71e638b888f59dd5ff3adf429e634
MAHEER-MAC\nerdares <maheer.ares@live.com>
2017-05-14 16:43:41 -0700

    Included algorithim instructions for calculating interception point

--- src/main/java/com/github/kaboomboom3/spacebattlearena/OtherAttackShipCommand.java
+++ src/main/java/com/github/kaboomboom3/spacebattlearena/OtherAttackShipCommand.java
@@ -2,88 +2,71 @@ package com.github.kaboomboom3.spacebattlearena;
 
 import ihs.apcs.spacebattle.ObjectStatus;
 import ihs.apcs.spacebattle.Point;
-import ihs.apcs.spacebattle.commands.*;
-
 
 /**
- * Created by nerdares on 5/12/2017.
- * An attack command that should correctly determine a good position to shoot the enemy ship.
+ * Created by nerdares on 5/14/2017.
  */
 public class OtherAttackShipCommand {
 
     public static final double TORPEDO_SPEED = 250;
-    public static final int ANGLE_TOLERANCE = 10;
+    public static final double ANGLE_TOLERANCE = 5;
 
-    public static Vector2D findInterceptVector(ObjectStatus ourShip, ObjectStatus enemyShip) {
+    public static Point finalPosition(ObjectStatus ship, ObjectStatus target) {
 
-
-        //<editor-fold desc="Initial values">
-
-        Vector2D shotOrigin = new Vector2D(new Point(0,0), ourShip.getPosition());
-        Vector2D targetOrigin = new Vector2D(new Point (0,0), enemyShip.getPosition());
-
-        Vector2D targetVel = new Vector2D(enemyShip.getSpeed() * Math.cos(enemyShip.getMovementDirection()),
-                enemyShip.getSpeed() * Math.cos(enemyShip.getMovementDirection()));
-
-        double targetRadius = enemyShip.getHitRadius();
-
+        //<editor-fold desc="Algorithm explanation">
+        /**
+         * Algorithm explanation :
+         * In order to calulate the point of interception to get the target, we need to split
+         * this problem into vectors. To do this :
+         * 1) We must first do everything relative to the ship.
+         *
+         * 2) We need to find a displacement vector from the ship to the target.
+         *
+         * 3) Then we need to find a vector which will be the target's displacement from the targets final position to
+         * the target's initial position. We will need to use the velocity of the target for this.
+         *
+         * 4) Then, we need to calculate the resultant vector which will be the
+         * the ships displacement + the target's displacement.
+         *
+         * 5) Then we will translate that resultant vector into world space and make our ship shoot to that point.
+         *
+         * The end result is to return a new point such that we take the relative x component and y component of the
+         * resultant vector into world space
+         *
+         * HOW TO SOLVE:
+         *
+         * The displacement vector is easy to calculate. The target's velocity vector is the problem. To calculate final
+         * position, we need to model it with simple physics equations. That is :
+         * Final position = Initial position + Velocity * time
+         * Notice this is a vector representation of the equation, which means this equation takes into account of both
+         * components. We use the Vector2D class to help us with that.
+         *
+         * Essentially, we want to do the initial position (as a vector) and add it to the target's velocity - that
+         * velocity vector must be scaled with time!
+         *
+         *
+         */
         //</editor-fold>
 
+        //Get the displacement vector from the ship to the target.
+        Vector2D displacement = new Vector2D(ship.getPosition(), target.getPosition());
 
-        Vector2D dirToTarget = Vector2D.toUnitVector(new Vector2D(ourShip.getPosition(), enemyShip.getPosition()));
-
-        Vector2D targetVelOrth = Vector2D.scale(dirToTarget, Vector2D.dotProduct(targetVel, dirToTarget));
-        Vector2D targetVelTan = Vector2D.subtract(targetVel, targetVelOrth);
-
-        Vector2D shotVelTan = Vector2D.copy(targetVelTan);
-
-        double shotVelSpeed = shotVelTan.magnitude();
+        //Get the velocity vector of the target. This will be scaled with time to represent the displacement vector
+        //between the target's initial position and the target's final position
+        Vector2D targetVelocity = new Vector2D(target.getSpeed() * Math.cos(target.getMovementDirection()),
+                target.getSpeed() * Math.sin(target.getMovementDirection()));
 
 
-        if(shotVelSpeed > TORPEDO_SPEED) {
-            return Vector2D.scale(Vector2D.toUnitVector(targetVel), TORPEDO_SPEED);
-        }
-        else {
-            double shotSpeedOrth = Math.sqrt(Math.pow(TORPEDO_SPEED, 2)) - Math.pow(shotVelSpeed, 2);
-            Vector2D shotVelOrth = Vector2D.scale(dirToTarget, shotSpeedOrth);
-            return Vector2D.add(shotVelOrth, shotVelTan);
-        }
-
+        //<editor-fold desc="End result notes">
+        /**
+         * End result will be
+         * World space translation of :
+         *      Vector sum of :
+         *          Displacement of ship to target + Displacement of ship's initial position to final position.
+         */
+        //</editor-fold>
+        return  null;
     }
 
-    /**
-     * Attacks the enemy ship by shooting at a predicted position.
-     * @param ourShip Our ship.
-     * @param enemyShip The enemy ship to target.
-     * @return Returns an Attack Command if our ship is in a valid rotation and position. Otherwise, returns a
-     * rotate command to fix angular position.
-     */
-    public static ShipCommand attackShip(ObjectStatus ourShip, ObjectStatus enemyShip) {
-        Point finalPosition = enemyShip.getPosition();
-
-        int relativeAngle = ourShip.getPosition().getAngleTo(finalPosition) - ourShip.getOrientation();
-
-        if (Math.abs(relativeAngle) - ANGLE_TOLERANCE < 0) {
-            return new FireTorpedoCommand('F');
-        } else {
-            return new RotateCommand(relativeAngle);
-        }
-    }
-
-    /**
-     * Logs necessary information of our ship and the enemy ship.
-     * @param ourShip Our ship.
-     * @param enemyShip The enemy ship to target.
-     * @param relativeAngle The relative angle from our ship (as the origin) to the enemy ship.
-     * @param finalPosition The predicted final position of the enemy ship.
-     */
-    public static void LogMessage(ObjectStatus ourShip, ObjectStatus enemyShip, int relativeAngle, Point finalPosition) {
-        System.out.println("Relative angle = " + relativeAngle);
-        System.out.println("Our position is : " + ourShip.getPosition());
-        System.out.println("Enemy position is : " + enemyShip.getPosition());
-        System.out.println("Enemy final position is : " + finalPosition);
-        System.out.println("Enemy speed is " + enemyShip.getSpeed());
-        System.out.println("----------------------END-----------------------");
-    }
 
 }
