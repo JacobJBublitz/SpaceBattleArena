ff96014d6f0a4d5ef9b0763847605998ae227c69
Aeron <246673@apps.everettsd.org>
2017-05-30 08:25:33 -0700

    Improving find the middle

--- src/main/java/com/github/kaboomboom3/spacebattlearena/Ship.java
+++ src/main/java/com/github/kaboomboom3/spacebattlearena/Ship.java
@@ -20,11 +20,11 @@ public class Ship extends BasicSpaceship {
     private static int worldHeight = 0;
 
     //Current game mode
-    private static Gamemodes gamemodes = Gamemodes.BAUBLE;
+    private static Gamemodes gamemodes = Gamemodes.FIND_THE_MIDDLE;
 
 
     public static void main(String[] args) {
-		TextClient.run("10.51.4.70", new Ship());
+		TextClient.run("localhost", new Ship());
 	}
 
 
@@ -38,7 +38,7 @@ public class Ship extends BasicSpaceship {
         return worldHeight;
     }
 
-    //</editor-fold>
+    //</editor-fold>    `
 
 
 
